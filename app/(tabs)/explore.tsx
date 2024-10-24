6d7bb9ed7769088dc94abecc9e96d0bd1815b96d
Rohit Hegade <60090023+Rohithegade02@users.noreply.github.com>
2024-10-24 19:53:17 +0530

    added images and icons

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 export default function TabTwoScreen() {
   return (
-    <View className="h-screen w-full bg-red-500">
-      <Text className="font-InterExtraBold">Hello</Text>
+    <View className="h-screen w-full m-10 ">
+      <Text className="font-InterBold">Hello</Text>
   </View>
   );
 }
---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+import calendar from "@/assets/icons/calendar.png";
+import logo from '@/assets/icons/logo.png';
+import man from '@/assets/icons/man.png';
+import home from '@/assets/icons/home.png';
+import article from '@/assets/icons/article.png';
+import chart from '@/assets/icons/chart.png';
+import graph from '@/assets/icons/graph.png';
+import company1 from '@/assets/icons/company-1.png';
+import company2 from '@/assets/icons/company-2.png';
+import company3 from '@/assets/icons/company-3.png';
+import company4 from '@/assets/icons/company-4.png';
+import company5 from '@/assets/icons/company-5.png';
+import company6 from '@/assets/icons/company-6.png';
+
+export const images = {
+    logo,
+    man,
+    company1,
+    company2,
+    company3,
+    company4,
+    company5,
+    company6
+};
+
+export const icons = {
+    calendar,
+    home,
+    article,
+    chart,
+    graph,
+};
